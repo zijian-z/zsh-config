94b3b405a0b2020ece07856d4a6aef0a18e0007a
zijian-z <zijian-zhang@outlook.com>
2022-05-07 20:28:46 +0800

    根据是否为mac选择配置文件位置

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
-cp .zshrc ~/
-cp manjaro-zsh-* /usr/share/zsh/
-cp p10k*.zsh /usr/share/zsh/
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    cp .zshrc-mac ~/.zshrc
+    cp manjaro-zsh* /usr/local/share/zsh/
+    cp p10k*.zsh /usr/local/share/zsh/
+else
+    cp .zshrc ~/
+    cp manjaro-zsh-* /usr/share/zsh/
+    cp p10k*.zsh /usr/share/zsh/
+fi
